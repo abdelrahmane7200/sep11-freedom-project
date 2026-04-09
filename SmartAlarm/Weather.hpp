351d539d373f4d87010914f9523657224f4e354b
Abdel <abdelrahmane7200@nycstudents.net>
2026-04-09 14:48:44 -0400

    Adjusted weather.cpp/hpp to work with an hour index and get me the specific data instead of just printing it to the serial.

--- SmartAlarm/Weather.hpp
+++ SmartAlarm/Weather.hpp
@@ -1,20 +1,27 @@
 #pragma once
-
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <Arduino.h>
 
 class Weather {
-  private:
+private:
     DynamicJsonDocument organizedWeatherData;
+    int currentHourIndex;
 
-  public:
-    // Constructor
+    String deriveCondition(float rain, float showers);
+
+public:
     Weather();
 
-    // Fetch weather JSON data from API
     void fetchWeatherData();
+    void updateCurrentHourIndex(const String& currentTime);
 
-    // Print the next 24 hours to Serial
+    // Getters for screen rendering
+    float getTemp(int offset = 0);          // offset from current hour
+    String getHourLabel(int offset = 0);    // e.g. "3PM"
+    String getCurrentCondition();
+    float getCurrentTemp();
+
+    // Legacy serial debug
     void printNext24Hours();
 };
